f91f081a250e0f64cfd310952272e86a0db512b2
Vinicius Ferreira <viniciusfe66@gmail.com>
2023-08-01 11:09:41 -0400

    inicio do sistema de contrato

---
+++ php/envioContrato.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?=
+ "Contrato de Serviço de Desenvolvimento de Sistema
+
+Este Contrato de Serviço de Desenvolvimento de Sistema ('Contrato') é celebrado em [data de início do contrato], entre [nome da empresa contratada], com sede em [endereço da empresa contratada], doravante denominada "Contratada", e [nome da empresa contratante], com sede em [endereço da empresa contratante], doravante denominada "Contratante".
+
+1. Objeto do Contrato
+
+1.1. A Contratada se compromete a desenvolver um sistema de software personalizado conforme as especificações e requisitos acordados com a Contratante.
+
+1.2. O sistema desenvolvido pela Contratada será entregue à Contratante na forma de [especificar a entrega do sistema, como software instalado, código-fonte, documentação, etc.].
+
+2. Escopo do Projeto
+
+2.1. O escopo detalhado do projeto, incluindo as funcionalidades e características do sistema, será descrito no documento de especificações técnicas acordado entre as partes.
+
+2.2. Quaisquer alterações no escopo do projeto deverão ser acordadas por escrito entre as partes, e poderão implicar em ajustes no prazo e no valor do contrato.
+
+3. Prazo de Entrega
+
+3.1. O prazo estimado para a entrega final do sistema é de [especificar o prazo em dias, semanas ou meses], a contar da data de assinatura deste contrato.
+
+3.2. A Contratada se compromete a envidar todos os esforços razoáveis para cumprir o prazo de entrega estabelecido. No entanto, atrasos decorrentes de eventos imprevistos, mudanças no escopo ou outras circunstâncias fora do controle da Contratada poderão impactar o prazo de entrega.
+
+4. Valor e Forma de Pagamento
+
+4.1. O valor total deste contrato é de [especificar o valor acordado], que será pago da seguinte forma:
+
+4.2. [especificar as etapas de pagamento, por exemplo: 30% do valor total no início do projeto, 40% na entrega de uma versão preliminar, 30% na entrega final do sistema].
+
+4.3. Todos os pagamentos deverão ser efetuados até a data de vencimento especificada no documento de fatura emitido pela Contratada.
+
+5. Propriedade Intelectual
+
+5.1. Todo o código-fonte, design, documentação e demais elementos desenvolvidos pela Contratada no âmbito deste projeto serão considerados propriedade intelectual exclusiva da Contratante.
+
+6. Confidencialidade
+
+6.1. Ambas as partes concordam em manter em sigilo todas as informações confidenciais e dados sensíveis compartilhados durante a execução deste contrato.
+
+7. Garantia
+
+7.1. A Contratada oferecerá um período de garantia de [especificar a duração da garantia] após a entrega final do sistema, durante o qual se compromete a corrigir eventuais defeitos e problemas no software desenvolvido.
+
+8. Rescisão
+
+8.1. Em caso de descumprimento de qualquer uma das cláusulas deste contrato por qualquer das partes, a outra parte poderá rescindi-lo, mediante aviso prévio por escrito, com efeito imediato.
+
+9. Lei Aplicável e Foro
+
+9.1. Este contrato será regido pelas leis do [especificar o país ou estado onde o contrato está sendo firmado].
+
+9.2. Fica eleito o foro da comarca de [especificar a cidade] para dirimir quaisquer questões decorrentes deste contrato.
+
+As partes declaram que leram, compreenderam e aceitaram todos os termos e condições estabelecidos neste Contrato, assinando-o em duas vias, de igual teor e forma, na presença de duas testemunhas.
+
+[Local], [Data]
+
+[Nome da Contratada] [Nome da Contratante]
+
+Testemunhas:
+
+    [Nome e assinatura]
+
+    [Nome e assinatura]
+
+[Observação: Este é um modelo básico de contrato de serviço de desenvolvimento de sistema. Recomendamos que você consulte um advogado especializado em contratos para ajustar o documento às suas necessidades específicas e garantir que esteja em conformidade com as leis e regulamentações do seu país ou estado.]";
+?>    
+</body>
+</html>
